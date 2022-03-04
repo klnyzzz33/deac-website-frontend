a4e8215d5bfba218acbdad9a320c2d954d7dc642
Ex0rc1st33 <72095082+Ex0rc1st33@users.noreply.github.com>
2022-03-04 17:16:30 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Form, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'deac-website-frontend';
+
+  constructor(private http: HttpClient) {}
+
+  onSubmit(form: NgForm) {
+    let data = form.form.value;
+    data["roles"] = ["ROLE_CLIENT"];
+
+    this.http.post(
+      'http://localhost:8080/api/register',
+      data,
+      {responseType: 'text'}
+    )
+    .subscribe(responseData => {
+      console.log(responseData);
+    });
+  }
 }
